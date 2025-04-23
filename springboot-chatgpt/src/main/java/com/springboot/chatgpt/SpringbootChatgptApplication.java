433d3105bc9ff4bb24f3c82571bf8cec8e85f4c3
sathvikmadarapu <sathvik.madarapu@gmail.com>
2025-04-23 13:23:55 -0500

    second-commit

--- springboot-chatgpt/src/main/java/com/springboot/chatgpt/SpringbootChatgptApplication.java
+++ springboot-chatgpt/src/main/java/com/springboot/chatgpt/SpringbootChatgptApplication.java
@@ -2,12 +2,21 @@ package com.springboot.chatgpt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+
+
 
 @SpringBootApplication
-public class SpringbootChatgptApplication {
+public class SpringbootChatgptApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootChatgptApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringbootChatgptApplication.class);
+	}
 }
